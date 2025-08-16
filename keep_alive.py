18c002d87899da237c42580e0a1daeae9bc86bd4
MachuPishtuu <jadkheir132@gmail.com>
2025-08-16 12:27:26 +0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,19 +1,12 @@
-from flask import Flask
-from threading import Thread
+from flask import Flask, Response
 
-app = Flask(__name__)
+app = Flask("keep_alive")
 
-@app.route('/')
-def index():
-    return "Bot is alive ✅", 200
-
-@app.route('/ping-x92a7f')
+@app.route("/ping-x92a7f", methods=["GET", "HEAD"])
 def ping():
-    return "Ping received ✅", 200
+    return Response("Bot is alive!", status=200)
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():  
-    t = Thread(target=run)
-    t.start()
+def keep_alive():
+    from threading import Thread
+    server = Thread(target=lambda: app.run(host="0.0.0.0", port=8080))
+    server.start()
